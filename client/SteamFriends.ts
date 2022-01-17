94e23b0faf7df18fb31688310ab139de900098c0
gurigraphics <gurigraphics@gmail.com>
2022-01-17 12:31:02 -0300

    Start create SteamFriends module

---
+++ client/SteamFriends.ts
@@ -0,0 +1,9 @@
+﻿import { Api, Verb } from './Api.js'
+
+export class SteamFriends {
+
+    public static async GetFriends(): Promise<any> {
+        return await Api.send(`/friends/friends`, Verb.Get)
+    }
+
+}
---
+++ server/Processors/SteamFriends.cs
@@ -0,0 +1,14 @@
+﻿using Airmiss.Processor;
+
+[ProcessorHub("friends")]
+public class SteamFriends
+{
+
+    [Processor("friends", Verb.Get)]
+    public object GetFriends()
+    {
+        return Steamworks.SteamFriends.GetFriends();
+    }
+
+}
+
