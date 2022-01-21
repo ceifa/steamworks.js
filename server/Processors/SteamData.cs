263840d4c7d54c41e3a1dedfc302e1d9772e5fdc
gurigraphics <gurigraphics@gmail.com>
2022-01-21 09:26:49 -0300

    add SteamMatchmaking and SteamData

---
+++ server/Processors/SteamData.cs
@@ -0,0 +1,35 @@
+﻿using Airmiss.Processor;
+
+[ProcessorHub("data")]
+public class SteamData
+{
+
+    [Processor("inviteFriend/{friendId}", Verb.Get)]
+    public static bool InviteFriend([Path("friendId")] ulong friendId)
+    {
+        var instanceLobby = new Steamworks.Data.Lobby();
+        return instanceLobby.InviteFriend(friendId);
+    }
+
+
+    /*
+    [ProcessorHub("data/lobby")]
+    public static class Lobby  
+    {
+        [Processor("data/lobby/inviteFriend", Verb.Get)]
+        public static bool InviteFriend([Path("friendId")] ulong friendId)
+        {
+            var instanceLobby = new Steamworks.Data.Lobby();          
+            return instanceLobby.InviteFriend(friendId);
+        }
+
+        [Processor("data/lobby/setFriendsOnly", Verb.Get)]
+        public static bool SetFriendsOnly()
+        {
+            var instanceLobby = new Steamworks.Data.Lobby();
+            return instanceLobby.SetFriendsOnly();
+        }
+    }
+
+    */
+}
---
+++ server/Processors/SteamMatchmaking.cs
@@ -0,0 +1,59 @@
+﻿using Airmiss.Processor;
+
+[ProcessorHub("matchmaking")]
+public class SteamMatchmaking
+{
+    [Processor("createLobbyAsync/{maxMembers}", Verb.Get)]
+    public async Task<bool> CreateLobbyAsync([Path("maxMembers")] int maxMembers)
+    {    
+        try
+        {
+            var createLobbyOutput = await Steamworks.SteamMatchmaking.CreateLobbyAsync(maxMembers);
+            if (!createLobbyOutput.HasValue)
+            {
+                Console.WriteLine("Lobby created but not correctly instantiated");
+                throw new Exception();
+            }
+
+            /*
+            LobbyPartnerDisconnected = false;
+            hostedMultiplayerLobby = createLobbyOutput.Value;
+            hostedMultiplayerLobby.SetPublic();
+            hostedMultiplayerLobby.SetJoinable(true);
+            hostedMultiplayerLobby.SetData(staticDataString, lobbyParameters)  
+                    currentLobby = hostedMultiplayerLobby;
+            */
+
+            return true;
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Failed to create multiplayer lobby");
+            Console.WriteLine(exception.ToString());
+            return false;
+        }
+
+    }
+
+    /*
+    void OnLobbyGameCreatedCallback(Lobby lobby, uint ip, ushort port, SteamId steamId)
+    {
+        AcceptP2P(OpponentSteamId); 
+    }
+
+    private void AcceptP2P(long opponentId)
+    {
+        try
+        {
+            // For two players to send P2P packets to each other, they each must call this on the other player
+            SteamNetworking.AcceptP2PSessionWithUser(opponentId);
+        }
+        catch
+        {
+            Debug.Log("Unable to accept P2P Session with user");
+        }
+    }
+    */
+
+}
+
