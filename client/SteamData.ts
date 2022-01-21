473866aeb6b6b3b3ed78bb989f43ac1a79a6a817
gurigraphics <gurigraphics@gmail.com>
2022-01-21 09:29:00 -0300

    add SteamMatchmaking.ts and SteamData.ts

---
+++ client/SteamData.ts
@@ -0,0 +1,12 @@
+﻿import { Api, Verb } from './Api.js'
+
+export namespace SteamData {
+
+    export class Lobby {
+
+        public static async InviteFriend(friendId: number): Promise<any> {
+            return await Api.send(`/data/inviteFriend/${friendId}`, Verb.Get)
+        }
+
+    }
+}
---
+++ client/SteamMatchmaking.ts
@@ -0,0 +1,9 @@
+﻿import { Api, Verb } from './Api.js'
+
+export class SteamMatchmaking {
+
+    public static async CreateLobbyAsync(maxMembers: number): Promise<any> {
+        return await Api.send(`/matchmaking/createLobbyAsync/${maxMembers}`, Verb.Get)
+    }
+
+}
