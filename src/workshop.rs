5906ea985c14294a6096d49f4ea85e74957cbac1
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2022-04-27 14:59:16 -0300

    fix formatting

--- src/workshop.rs
+++ src/workshop.rs
@@ -218,7 +218,7 @@ pub mod workshop {
             None => None,
         }
     }
-    
+
     /// Download or update a workshop item.
     /// If high priority is true, start the download in high priority mode, pausing any existing in-progress Steam downloads and immediately begin downloading this workshop item.
     /// @returns boolean
