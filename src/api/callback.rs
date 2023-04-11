ceifa/steamworks.js

commit f27b4a86df4569c5f87a511da2d4049f6037d20f

Add callback api

src/api/callback.rs
@@ -1,0 +1,82 @@
+use napi_derive::napi;
+
+#[napi]
+pub mod callback {
+    use napi::{
+        bindgen_prelude::ToNapiValue,
+        threadsafe_function::{ErrorStrategy, ThreadsafeFunction, ThreadsafeFunctionCallMode},
+        JsFunction,
+    };
+    use steamworks::{
+        CallbackHandle, PersonaStateChange, SteamServerConnectFailure, SteamServersConnected,
+        SteamServersDisconnected,
+    };
+
+    #[napi]
+    pub struct Handle {
+        handle: Option<CallbackHandle>,
+    }
+
+    #[napi]
+    impl Handle {
+        #[napi]
+        pub fn disconnect(&mut self) {
+            self.handle = None;
+        }
+    }
+
+    #[napi]
+    pub enum SteamCallback {
+        PersonaStateChange,
+        SteamServersConnected,
+        SteamServersDisconnected,
+        SteamServerConnectFailure,
+    }
+
+    #[napi(ts_generic_types = "C extends keyof import('./callbacks').CallbackReturns")]
+    pub fn register(
+        steam_callback: SteamCallback,
+        #[napi(ts_arg_type = "(value: import('./callbacks').CallbackReturns[C]) => void")]
+        handler: JsFunction,
+    ) -> Handle {
+        let threadsafe_handler: ThreadsafeFunction<serde_json::Value, ErrorStrategy::Fatal> =
+            handler
+                .create_threadsafe_function(0, |ctx| Ok(vec![ctx.value]))
+                .unwrap();
+
+        let handle = match steam_callback {
+            SteamCallback::PersonaStateChange => {
+                register_callback::<PersonaStateChange>(threadsafe_handler)
+            }
+            SteamCallback::SteamServersConnected => {
+                register_callback::<SteamServersConnected>(threadsafe_handler)
+            }
+            SteamCallback::SteamServersDisconnected => {
+                register_callback::<SteamServersDisconnected>(threadsafe_handler)
+            }
+            SteamCallback::SteamServerConnectFailure => {
+                register_callback::<SteamServerConnectFailure>(threadsafe_handler)
+            }
+        };
+
+        Handle {
+            handle: Some(handle),
+        }
+    }
+
+    fn register_callback<C>(
+        threadsafe_handler: ThreadsafeFunction<serde_json::Value, ErrorStrategy::Fatal>,
+    ) -> CallbackHandle
+    where
+        C: steamworks::Callback + serde::Serialize,
+    {
+        let client = crate::client::get_client();
+        client.register_callback(move |value: C| {
+            let value = serde_json::to_value(&value).unwrap();
+            threadsafe_handler.call(
+                serde_json::to_value(value).unwrap(),
+                ThreadsafeFunctionCallMode::Blocking,
+            );
+        })
+    }
+}

.gitignore
@@ -1,3 +1,4 @@
 /target
 /dist
 /node_modules
+Cargo.lock

.prettierrc
@@ -1,0 +1,9 @@
+{
+    "singleQuote": true,
+    "trailingComma": "all",
+    "endOfLine": "lf",
+    "quoteProps": "consistent",
+    "semi": false,
+    "printWidth": 140,
+    "tabWidth": 4
+}

Cargo.toml
@@ -7,12 +7,13 @@ edition = "2021"
 crate-type = ["cdylib"]
 
 [dependencies]
-napi = { version = "2", features = ["tokio_rt", "napi6"] }
+napi = { version = "2", features = ["tokio_rt", "napi6", "serde-json"] }
 napi-derive = "2"
 lazy_static = "1"
 tokio = { version = "1", features = ["sync", "time"] }
-# TODO: Change to the official crate release
-steamworks = { git = "https://github.com/Noxime/steamworks-rs.git" }
+steamworks = { git = "https://github.com/Noxime/steamworks-rs.git", features = ["serde"]}
+serde = "1"
+serde_json = "1"
 
 [build-dependencies]
 napi-build = "2"

callbacks.d.ts
@@ -1,0 +1,16 @@
+import client = require('./client')
+
+export interface CallbackReturns {
+    [client.callback.SteamCallback.PersonaStateChange]: {
+        steam_id: bigint
+        flags: { bits: number }
+    }
+    [client.callback.SteamCallback.SteamServersConnected]: {}
+    [client.callback.SteamCallback.SteamServersDisconnected]: {
+        reason: number
+    }
+    [client.callback.SteamCallback.SteamServerConnectFailure]: {
+        reason: number
+        still_retrying: boolean
+    }
+}

client.d.ts
@@ -127,3 +127,15 @@ export namespace workshop {
    */
   export function download(itemId: bigint, highPriority: boolean): boolean
 }
+export namespace callback {
+  export const enum SteamCallback {
+    PersonaStateChange = 0,
+    SteamServersConnected = 1,
+    SteamServersDisconnected = 2,
+    SteamServerConnectFailure = 3
+  }
+  export function register<C extends keyof import('./callbacks').CallbackReturns>(steamCallback: SteamCallback, handler: (value: import('./callbacks').CallbackReturns[C]) => void): Handle
+  export class Handle {
+    disconnect(): void
+  }
+}

index.js
@@ -1,7 +1,7 @@
 const { platform, arch } = process
 
 /** @typedef {typeof import('./client.d')} Client */
-/** @type {Client | undefined} */
+/** @type {Client} */
 let nativeBinding = undefined
 
 if (platform === 'win32' && arch === 'x64') {
@@ -40,3 +40,5 @@ module.exports.init = (appId) => {
 
     return api
 }
+
+module.exports.SteamCallback = nativeBinding.callback.SteamCallback;

src/api/mod.rs
@@ -5,3 +5,4 @@ pub mod input;
 pub mod localplayer;
 pub mod stats;
 pub mod workshop;
+pub mod callback;

commit 9224ffe62cdc827c7278049aeae89ce6864f9c9b

Fix broken build.
Fixes #80.

According to git bisect, the build was broken in revision f27b4a86df4569c5f87a511da2d4049f6037d20f, which added `callback.rs`.

Specifically, `callback.rs` added a `pub enum SteamCallback`. Any NAPI-RS file that exports an enum requires you to `use napi::bindgen_prelude::ToNapiValue`, which it did, but it also requires you to use `FromNapiValue`.

As far as I can tell, this has always been true, even back at revision f27b4a86. The parent revision to f27b4a86 deleted `Cargo.lock`, but even when I brought `Cargo.lock` back, it _still_ required `FromNapiValue`.

So, I have no idea why the build has been working at all for the last year, but this definitely seems to fix it.

src/api/callback.rs
@@ -3,7 +3,7 @@ use napi_derive::napi;
 #[napi]
 pub mod callback {
     use napi::{
-        bindgen_prelude::ToNapiValue,
+        bindgen_prelude::{FromNapiValue, ToNapiValue},
         threadsafe_function::{ErrorStrategy, ThreadsafeFunction, ThreadsafeFunctionCallMode},
         JsFunction,
     };

src/api/matchmaking.rs
@@ -3,7 +3,7 @@ use napi_derive::napi;
 #[napi]
 pub mod matchmaking {
     use crate::api::localplayer::PlayerSteamId;
-    use napi::bindgen_prelude::{BigInt, Error, ToNapiValue};
+    use napi::bindgen_prelude::{BigInt, Error, FromNapiValue, ToNapiValue};
     use std::collections::HashMap;
     use steamworks::LobbyId;
     use tokio::sync::oneshot;

src/api/networking.rs
@@ -3,7 +3,7 @@ use napi_derive::napi;
 #[napi]
 pub mod networking {
     use napi::{
-        bindgen_prelude::{BigInt, Buffer, ToNapiValue},
+        bindgen_prelude::{BigInt, Buffer, FromNapiValue, ToNapiValue},
         Error,
     };
     use steamworks::SteamId;