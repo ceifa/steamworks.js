375d800ba3ee5b9249a7941461f39b5ee794dc44
ceifa <gabrinelson27@gmail.com>
2023-04-23 17:53:47 -0300

    fix build files location on cross-build

--- build.js
+++ build.js
@@ -2,32 +2,42 @@ const path = require('node:path');
 const fs = require('node:fs');
 const child_process = require('node:child_process');
 
-const copy = (source, dest) => {
+const targets = {
+    ['x86_64-pc-windows-msvc']: {
+        folder: 'win64',
+        files: ['steam_api64.dll', 'steam_api64.lib'],
+        platform: 'win32',
+        arch: 'x64'
+    },
+    ['x86_64-unknown-linux-gnu']: {
+        folder: 'linux64',
+        files: ['libsteam_api.so'],
+        platform: 'linux',
+        arch: 'x64'
+    },
+    ['x86_64-apple-darwin']: {
+        folder: 'osx',
+        files: ['libsteam_api.dylib'],
+        platform: 'darwin',
+        arch: 'x64'
+    },
+    ['aarch64-apple-darwin']: {
+        folder: 'osx',
+        files: ['libsteam_api.dylib'],
+        platform: 'darwin',
+        arch: 'arm64'
+    }
+}
+
+const target = targets[process.argv.at(-1)] || Object.values(targets).find(t => t.platform === process.platform && t.arch === process.arch)
+
+const dist = path.join(__dirname, 'dist', target.folder)
+const redist = path.join(__dirname, 'sdk/redistributable_bin', target.folder)
+target.files.forEach(file => {
+    const [source, dest] = [path.join(redist, file), path.join(dist, file)]
     try { fs.mkdirSync(path.dirname(dest), { recursive: true }) } catch { }
     fs.copyFileSync(source, dest)
-}
-
-const { platform, arch } = process
-
-let files = []
-let folder = undefined
-
-if (platform === 'win32' && arch === 'x64') {
-    folder = 'win64'
-    files = ['steam_api64.dll', 'steam_api64.lib']
-} else if (platform === 'linux' && arch === 'x64') {
-    folder = 'linux64'
-    files = ['libsteam_api.so']
-} else if (platform === 'darwin') {
-    folder = 'osx'
-    files = ['libsteam_api.dylib']
-} else {
-    throw new Error(`Unsupported OS: ${platform}, architecture: ${arch}`)
-}
-
-const dist = path.join(__dirname, 'dist', folder)
-const redist = path.join(__dirname, 'sdk/redistributable_bin', folder)
-files.forEach(file => copy(path.join(redist, file), path.join(dist, file)))
+})
 
 const relative = path.relative(process.cwd(), dist)
 const params = [
