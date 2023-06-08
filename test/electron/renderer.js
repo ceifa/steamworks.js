e030694c6c8e3986b999470cd4d1f1fc35efcd93
Dan Fabulich <dan@fabulich.com>
2023-06-08 16:59:13 -0700

    Add a button to test the overlay

--- test/electron/renderer.js
+++ test/electron/renderer.js
@@ -4,3 +4,7 @@ const client = steamworks.init(480);
 
 const playerName = client.localplayer.getName()
 document.getElementById('name').innerText = playerName
+
+document.getElementById('activateOverlay').addEventListener('click', function() {
+    client.overlay.activateToWebPage('https://www.example.com/')
+})
